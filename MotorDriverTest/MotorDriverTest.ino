94adcd4607054b87ddf996a811f52b90d520d220
Aaron Wilhelm <aaron.j.wilhelm@gmail.com>
2017-05-08 13:41:04 -0700

    Revised test so that we also have asynchronous motor testing

--- MotorDriverTest/MotorDriverTest.ino
+++ MotorDriverTest/MotorDriverTest.ino
@@ -15,30 +15,51 @@ void setup() {
   pinMode(RIn2, OUTPUT);
   pinMode(LIn3, OUTPUT);
   pinMode(LIn4, OUTPUT);
-  digitalWrite(RIn1, forward);
-  digitalWrite(LIn3, forward);
-  digitalWrite(RIn2, !forward);
-  digitalWrite(LIn4, !forward);
   
 }
 
 void loop() {
-  
-  for(int j = 0; j <= 255; j++){
-    analogWrite(REnable, j);
-    analogWrite(LEnable, j);
-    delay(10);
-  }
-  for(int j = 255; j >= 0; j--){
-    analogWrite(REnable, j);
-    analogWrite(LEnable, j);
-    delay(10);
-  }
-
-  forward = !forward;
-  
   digitalWrite(RIn1, forward);
   digitalWrite(LIn3, forward);
   digitalWrite(RIn2, !forward);
   digitalWrite(LIn4, !forward);
+  //go forward and backwards with both wheels
+  for(int k = 0; k < 2; k++){
+     //increase the speeds
+    for(int j = 0; j <= 255; j++){
+      analogWrite(REnable, j);
+      analogWrite(LEnable, j);
+      delay(10);
+    }
+    //decrease the speeds
+    for(int j = 255; j >= 0; j--){
+      analogWrite(REnable, j);
+      analogWrite(LEnable, j);
+      delay(10);
+    }
+    //go backwards
+    forward = !forward;
+  }
+
+  //do the same thing but now have left wheel increase/decrease speed at half the rate and in the opposite direction
+    digitalWrite(RIn1, forward);
+    digitalWrite(LIn3, !forward);
+    digitalWrite(RIn2, !forward);
+    digitalWrite(LIn4, forward);
+ for(int k = 0; k < 2; k++){
+     //increase the speeds
+    for(int j = 0; j <= 255; j++){
+      analogWrite(REnable, j);
+      analogWrite(LEnable, j/2);
+      delay(10);
+    }
+    //decrease the speeds
+    for(int j = 255; j >= 0; j--){
+      analogWrite(REnable, j);
+      analogWrite(LEnable, j/2);
+      delay(10);
+    }
+    //go backwards
+    forward = !forward;
+  }
 }
